e1e5933aef0aa33d95c3f2cdb8acd0d8c7496800
Mohit Kumar <kumar.mohit983@gmail.com>
2017-02-26 16:59:36 +0530

    Complete API

--- commands.js
+++ commands.js
@@ -14,12 +14,21 @@ connection.connect(function(err) {
         console.log("Connected");
 });
 
+function selectdata(res,name) {
+	connection.query("select* from cust where custname = '"+name + "';",function(err,result) {
+		if(err) {
+			console.log(err.stack);
+		} else {
+			console.log(result);
+			res.send(result);
+		}
+	});
+};
 
 function insertdata(id,name,mob,res) {
 	connection.query("insert into cust (custid,custname,custmobile) values (" + id + ",'" + name + "'," + mob + ");",function(err,result) {
 		if(err) {
 			console.log(err.stack);
-			return;
 		} else {
 			console.log(result);
 		}	
@@ -40,7 +49,6 @@ function deletequery(id,res) {
 	connection.query("delete from cust where custid = " + id + ";" ,function (err,result) {
 		if(err) {
 			console.log(err.stack);
-			return;
 		} else {
 			console.log(result);
 		}
@@ -50,6 +58,5 @@ function deletequery(id,res) {
 module.exports.insertd = insertdata;
 module.exports.deleted = deletequery;
 module.exports.updated = updatedata;
-
-
+module.exports.selectd = selectdata;
 
--- routes.js
+++ routes.js
@@ -4,17 +4,26 @@ module.exports = {
 	configure: function(app) {
 		app.post('/commands/',function(req,res) {
 			console.log(req.body.mob);
+			res.send(req.body);
 			command.insertd(req.body.id,req.body.name,req.body.mob,res);
 		});
 
 		app.post('/update/',function(req,res) {
 			console.log(req.body.name);
+			res.send(body);
 			command.updated(req.body.id,req.body.name,res);
 		});
 
 		app.post('/delete/',function(req,res) {
 			console.log(req.body.id);
+			res.send(body);
 			command.deleted(req.body.id,res);
+		});
+
+		app.post('/select/',function(req,res) {
+			console.log(req.body.name);
+			//res.send(req.body);
+			command.selectd(res,req.body.name);
 		});
 	}
 }
